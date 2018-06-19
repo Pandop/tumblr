611a68b1c2f25929f570744318ba8c5b218800ce
Pandop <s.kel@student.qut.edu.au>
2018-06-19 18:41:46 +1000

    Add edit and destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,8 +24,26 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:success] = "Post was updated successfully."
+      redirect_to(post_path)
+      
+    else
+      render('edit')
+    end
+  end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to(posts_path)
+  end
   private
     def post_params
       params.require(:post).permit(:title, :body)
