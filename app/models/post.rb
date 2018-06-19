518e3707c5a11bad6f95207c8ad0852a02340868
Pandop <s.kel@student.qut.edu.au>
2018-06-19 19:33:10 +1000

    Add page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
 
+  # Ensure comments are delete when a post is delete
   has_many :comments, dependent: :destroy
+  
   # Validations
   # validates :title, presence: true, length:{minimum: 5}
   # validates :body, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'posts#index'
   
+  get "/about", to: "pages#about"
+
   resources :posts do 
     resources :comments
   end
